020fce38e11bce6f11ed0627c74fc9740f6bd078
haiz <haiz.gigs@gmail.com>
2023-05-10 09:52:33 +0000

    updated site

--- docs/client/assets/entries/renderer_error.page.da2f52e2.js
+++
@@ -1,1 +0,0 @@
-import{a as n,F as r,j as e}from"../chunks/chunk-b7c514cf.js";function t({is404:o}){return o?n(r,{children:[e("h1",{children:"404 Page Not Found"}),e("p",{children:"This page could not be found."})]}):n(r,{children:[e("h1",{children:"500 Internal Error"}),e("p",{children:"Something went wrong."})]})}export{t as Page};
